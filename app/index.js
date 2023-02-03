8dccf678eec1d0533ead05259fa8c20560a09760
Hans Gmelig meyling <hans.gmelig.meyling@oliver-it.nl>
2023-02-03 11:58:21 +0100

    Stage balls loading again

--- app/index.js
+++ app/index.js
@@ -4,21 +4,21 @@ const path = require("path");
 
 const app = express();
 app.use(express.json());
-app.use("/web", express.static(path.join(__dirname, "./webapp")));
-console.log("web to /webapp")
+app.use("/web", express.static(path.join(__dirname, "./web3")));
+console.log("web to /web3")
 
+/*
 app.get("/hiFolks", function(req, res) {
     res.send({
         message: "Hi there!"
     });
 });
+*/
 
-
-
-//const ieApi = require("./oehp-ie-api");
-//const ieEngine = require("./oehp-ie-engine");
-//ieEngine.app(app);
-//ieApi.api(app, ieEngine.configuration, ieEngine.engine);
+const ieApi = require("./oehp-ie-api");
+const ieEngine = require("./oehp-ie-engine");
+ieEngine.app(app);
+ieApi.api(app, ieEngine.configuration, ieEngine.engine);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
